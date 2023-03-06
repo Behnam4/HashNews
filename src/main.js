cae1e17ba0253f9fc484be4582ccd552dae7ccf7
Behnam <behnam.amiri@gitkraken.com>
2023-03-06 13:24:53 -0700

    Load the html file

--- src/main.js
+++ src/main.js
@@ -4,12 +4,23 @@ const { app, BrowserWindow } = require('electron');
 
 const createWindow = () => {
   const win = new BrowserWindow({
-    backgroundColor: '#1F000E',
+    backgroundColor: '#778884',
     width: 800,
-    height: 600
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
   });
 
-  win.loadFile('main.html');
+  win.loadURL(`file://${__dirname}/main.html`);
+  // win.loadFile('main.html');
+
+  // The reason why win.loadFile does not work for this repository is that it is an
+  // Electron app packaged as a single executable file using electron-builder. When
+  // an Electron app is packaged as a single executable file, the app's resources
+  // (such as the HTML, CSS, and JavaScript files) are no longer located in individual
+  // files on disk. Instead, they are embedded in the executable file itself as a compressed
+  // archive. Therefore, the win.loadFile method cannot find the HTML file to load.
 };
 
 app.whenReady().then(() => {
